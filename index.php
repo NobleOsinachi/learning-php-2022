897c7d54deff317ce40f0dc86844f8324825af8f
Noble Osinachi <56831744+NobleOsinachi@users.noreply.github.com>
2022-02-04 14:54:36 +0100

    Skeleton template

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+
+<?php
+
+
+
+$name = "<i>Noble</i>";
+
+
+
+
+echo("<h1>$name</h1>");
+
+
+echo("Hello World");
+?>
+
+</body>
+</html>
